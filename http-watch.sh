4264c82cf1e3061122e7b3a01a156989f9ad06cc
Thomas Keast <tkeast@cloudabove.com>
2019-01-10 15:16:44 +0000

    Adding testing for multiple instances

--- http-watch.sh
+++ http-watch.sh
@@ -27,11 +27,37 @@ function LOG_DATE {
 log_running_date=`date '+%Y-%m-%d %H:%M:%S'`
 }
 
+function LIST_MODE {
+echo -e "\e[92mList mode detected... Running now...\e[0m"
+LIST_RUN=1
+if [ -f ./$LIST_NAME ]
+then
+    LIST_CS=$(cat ./$LIST_NAME)
+else
+	echo "I can't seem to fidnt he file..."
+	read -p "Would you like me to make one? [y/n] - " MKLIST
+	if [ $MKLIST == "y" ] || [ $MKLIST == "Y" ]
+	then
+		touch ./$LIST_NAME
+		echo "File created, please add some websites and re-run the script..."
+		exit 0
+	else
+		echo "Please run the normal script if you wish to monitor a single website..."
+		exit 1
+	fi
+
+fi
+
+
+}
+
 # Pre-Script Checks
 
-if [ ! -d "$LOG_PATH" ] # Check to see if the path exists
+if [[ ! -z "$1" ]] && [[ `echo $1` == '--list' ]] || [[ `echo $1` == '-lrun' ]]
 then
-	mkdir -p $LOG_PATH # If it does not, create the path from the config
+        LIST_MODE
+else
+		LIST_RUN=0
 fi
 
 touch $LOG_PATH/http-watch.log  # Create a new log file
@@ -44,7 +70,12 @@ LOG_DATE # Get current time with Hours, Mins, Seconds
 echo "Script executed: $log_running_date" >> $LOG_FILE # Write execute time to log
 echo "User running: `whoami`" >> $LOG_FILE # Write user that executed the script to the log
 
-echo -e "\nHTTP-WATCH is monitoring: $URL"
+if [ $LIST_RUN == "1" ]
+then
+	echo -e "\nHTTP-WATCH is monitoring the following list: $LIST_SH"
+else
+	echo -e "\nHTTP-WATCH is monitoring: $URL"
+fi
 
 TESTURL # Performs intial test to see if URL is online and get its status code
 
